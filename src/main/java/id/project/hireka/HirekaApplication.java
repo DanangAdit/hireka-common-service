875ccf2ae257ec593ec82a7731841eb8b0f3e851
danang.wicaksana <danang.wicaksana@bankmandiri.co.id>
2025-02-14 15:26:29 +0700

    Initial commit for Hireka project

---
+++ src/main/java/id/project/hireka/HirekaApplication.java
@@ -0,0 +1,13 @@
+package id.project.hireka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirekaApplication.class, args);
+	}
+
+}
